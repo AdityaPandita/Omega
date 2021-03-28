9ed138c264bfde49fd286453ace6407b97959b68
Rithik <rithik.pandita.143@gmail.com>
2021-03-28 22:22:10 +0530

    Initial Omegle Commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Component } from 'react';
+
+import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+class App extends Component {
+  render() {
+    return <WebView
+          source={{ uri: 'https://www.omegle.com/' }}
+         />;
+  }
+}
+
+export default App;
